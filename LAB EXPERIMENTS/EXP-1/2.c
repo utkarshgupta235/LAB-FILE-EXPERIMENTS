467fc003121df7aa3571530899025d75587a3db2
utkarshgupta235 <guptautkarsh227@gmail.com>
2025-09-20 17:29:30 +0530

    Update 2.c

--- LAB EXPERIMENTS/EXP-1/2.c
+++ LAB EXPERIMENTS/EXP-1/2.c
@@ -1,8 +1,9 @@
-//	Write a C program to print “ Hello World”
+//	2.	Write a C Program to print the address in multiple lines (new line).
 
 #include<stdio.h>
 
 int main(){
 printf("119/501\nDarshan Purwa\nKanpur\nUttar Pradesh");
 return 0;
+
 }

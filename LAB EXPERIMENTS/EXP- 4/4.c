396b94bead1b9ccc8a2a75586c635760f618c161
UTKARSH GUPTA <guptautkarsh227@gmail.com>
2025-09-28 19:49:52 +0530

    Update 4.c

--- LAB EXPERIMENTS/EXP- 4/4.c
+++ LAB EXPERIMENTS/EXP- 4/4.c
@@ -1,4 +1,4 @@
-// 4.	Declare a static local variable inside a function. Observe how its value persists across function calls.
+// 	Declare a static local variable inside a function. Observe how its value persists across function calls.
 #include <stdio.h>
 void function() {
     static int staticVar = 0; // Static local variable
@@ -13,4 +13,5 @@ int main() {
     function(); 
     printf("Call 3\n");
     return 0;
+
 }
